7807c0d675eb6fd643232f08f80ee1a38d8984eb
fernando <fdsantosj15@gmail.com>
2025-11-25 11:45:43 -0300

    erro concertado

--- trabalhoc/concessionaria/urls.py
+++ trabalhoc/concessionaria/urls.py
@@ -2,6 +2,7 @@ from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import CarroViewSet
 
+
 router = DefaultRouter()
 router.register(r'carros', CarroViewSet)
 
